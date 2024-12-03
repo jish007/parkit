c8f9dff8c19b0dd0ec56f116d994656a709754ad
gokulg7 <07gokulg@gmail.com>
2024-12-03 23:38:39 +0530

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:park_it/admin/screens/Auth/admin_login_screen.dart';
 import 'package:park_it/super_admin/screens/MainMenu/super_admin_main_menu.dart';
 
 Future<void> main() async {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.system,
-        home: SuperAdmin(),
+        home:  AdminLogin(),//SuperAdmin(),
     );
   }
 }
